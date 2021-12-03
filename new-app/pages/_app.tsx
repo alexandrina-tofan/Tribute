7c41e622a6a5a56a56189c941f72c1b0d514f6ee
Alexandrina <alexandrinatofan@yahoo.com>
2021-12-03 13:41:28 +0200

    Structurarea pe componente

--- new-app/pages/_app.tsx
+++ new-app/pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import React, { useEffect, FC } from "react";
+import { AppProps } from "next/app";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+import { EmotionCache } from "@emotion/cache";
+import createEmotionCache from "./createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+interface MyAppProps extends AppProps {
+  // eslint-disable-next-line react/require-default-props
+  emotionCache?: EmotionCache;
 }
-
-export default MyApp
+const App: FC<MyAppProps> = ({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+export default App;
---
+++ new-app/pages/theme.ts
@@ -0,0 +1,16 @@
+
+import { createTheme } from '@mui/material/styles';
+
+const palette = {
+  primary: {
+    main: '#3A5AF9',
+  }
+};
+
+export const theme = createTheme({
+  palette: { ...palette },
+  typography: {
+    fontFamily: '"Epilogue", "Helvetica", "Arial", sans-serif',
+    fontSize: 14,
+  },
+});
