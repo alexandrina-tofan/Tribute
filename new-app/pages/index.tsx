eb02136112e775a97bfd4b9304d80257245d3af2
Alexandrina <alexandrinatofan@yahoo.com>
2021-12-03 13:51:42 +0200

    changes

--- new-app/pages/index.tsx
+++ new-app/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Image from 'next/image';
 import Page_one from '../components/Page_1';
-import Head from '../components/Head';
 import Page_two from '../components/Page_2';
 import { styled } from '@mui/system';
 import Page_three from '../components/Page_3';
@@ -12,7 +11,6 @@ import Page_six from '../components/Page_6';
 const Home: NextPage = () => {
     return (
       <div >
-        <Head />
         <Page_one />
         <Page_two />
         <Page_three />
